0bf2fafaf0db42817dc7578a3410eef286ca5924
baicheng <baicheng.wang@owitho.com>
2019-03-16 17:51:16 +0800

    finish demo

--- src/main/java/com/baicheng/springbucks/SpringBucksApplication.java
+++ src/main/java/com/baicheng/springbucks/SpringBucksApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -21,6 +22,7 @@ import java.util.Optional;
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 @Slf4j
 public class SpringBucksApplication implements ApplicationRunner {
 
